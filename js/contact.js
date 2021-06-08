77bf1ba4448641c2f1f3bab0268a3f7495aa1825
rakshitweb <56441869+rakshitweb@users.noreply.github.com>
2021-06-08 09:11:54 +0530

    Add files via upload

--- js/contact.js
+++ js/contact.js
@@ -1,23 +1,24 @@
-let shownContact = false;
+// let shownContact = false;
 window.addEventListener("scroll", () => {
   let navAbout = document.querySelectorAll("nav div span")[4];
-  let element = document.querySelector("#project .container");
+  let element = document.querySelector("#contact");
   let position = element.getBoundingClientRect();
-  // console.log(
-  //   window.scrollY,
-  //   window.innerHeight,
-  //   position.top,
-  //   position.bottom
-  // );
-  if (
-    2 * position.top < window.innerHeight &&
-    position.bottom >= 0 &&
-    !shownContact
-  ) {
-    element.style.opacity = 1;
-    shownContact = true;
-  }
-  if (position.top <= 145 && position.bottom > 80) {
+  console.log(
+    window.scrollY,
+    window.innerHeight,
+    position.top,
+    position.bottom
+  );
+  // if (
+  //   2 * position.top < window.innerHeight &&
+  //   position.bottom >= 0 &&
+  //   !shownContact
+  // ) {
+  //   element.style.opacity = 1;
+  //   shownContact = true;
+  // }
+  if (position.top <= 145) {
+    console.log(navAbout);
     navAbout.classList.add("activeClass");
   } else {
     navAbout.classList.remove("activeClass");
